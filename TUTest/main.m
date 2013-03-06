befb0e2b8eb3deeb5b4423267264e21fb0d69626
Martijn de Vos <mdmartijndevos@gmail.com>
2013-03-06 22:47:42 +0100

    Initial Commit

---
+++ TUTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TUTest
+//
+//  Created by Martijn de Vos on 06-03-13.
+//  Copyright (c) 2013 Martijn de Vos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
